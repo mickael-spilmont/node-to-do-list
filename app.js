a9f497c9e34ad2b3338ab1b79b987993c3b50851
Mickael <mickael.spilmont.fr@gmail.com>
2019-10-24 16:08:02 +0200

    Adding ejs home page

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const session = require('session');
+// const session = require('session');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extend: false });
 
+const toDoList = [];
+
 const app = express();
 
+app.get('/todo', (req, res) => {
+    res.render('todo.ejs');
+});
+
+app.listen(8080);
