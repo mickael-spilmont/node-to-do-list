3f8165642881bb1c747b3860eac34c1641cfe299
Mickael <mickael.spilmont.fr@gmail.com>
2019-10-24 17:58:40 +0200

    Adding toDo.ejs, with display of to do list, and basic form for add task

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extend: false });
 
-const toDoList = [];
-
 const app = express();
+app.set('view engine', 'ejs');
+
+const toDoList = [
+    'Show the to do list',
+    'Add thing to do',
+    'remove thing to do'
+];
 
 app.get('/todo', (req, res) => {
-    res.render('todo.ejs');
-});
+    res.render('todo.ejs', { toDoList: toDoList });
+})
+.post('/todo/add', (req, res) => {
+    toDoList.push('Add');
+    res.render('todo.ejs', { toDoList: toDoList });
+})
 
 app.listen(8080);
