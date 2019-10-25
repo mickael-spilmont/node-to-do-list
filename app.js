4d53912bb6a3460e66409f32af2d480250f9d2ca
Mickael <mickael.spilmont.fr@gmail.com>
2019-10-25 22:30:24 +0200

    Implementation of cookieSession to keep toDoList data

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-// const session = require('session');
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extend: false });
+const cookieSession = require('cookie-session');
+// const bodyParser = require('body-parser');
+// const urlencodedParser = bodyParser.urlencoded({ extend: false });
 
 const app = express();
 app.set('view engine', 'ejs');
 
-const toDoList = [
-    'Show the to do list',
-    'Add thing to do',
-    'remove thing to do'
-];
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1']
+}))
 
-app.get('/todo', (req, res) => {
-    res.render('todo.ejs', { toDoList: toDoList });
+app.use((req, res, next) => {
+    req.session.thingsToDo = (req.session.thingsToDo || []);
+    next();
 })
-.post('/todo/add', (req, res) => {
-    toDoList.push('Add');
-    res.render('todo.ejs', { toDoList: toDoList });
+
+app.get('/todo', (req, res, next) => {
+    res.render('todo.ejs', { thingsToDo: req.session.thingsToDo });
+})
+.post('/todo/add', (req, res, next) => {
+    req.session.thingsToDo.push('Add');
+    res.render('todo.ejs', { thingsToDo: req.session.thingsToDo });
 })
 
 app.listen(8080);
