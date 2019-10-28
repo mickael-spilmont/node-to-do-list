2828c411856bca3a7190dcd334d8495479e51393
Mickael <mickael.spilmont.fr@gmail.com>
2019-10-28 19:38:01 +0100

    Adding functionalitys of : * adding task * removing task

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
-// const bodyParser = require('body-parser');
-// const urlencodedParser = bodyParser.urlencoded({ extend: false });
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extend: false });
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -19,8 +19,14 @@ app.use((req, res, next) => {
 app.get('/todo', (req, res, next) => {
     res.render('todo.ejs', { thingsToDo: req.session.thingsToDo });
 })
-.post('/todo/add', (req, res, next) => {
-    req.session.thingsToDo.push('Add');
+.post('/todo/add', urlencodedParser , (req, res, next) => {
+    console.log(req.body.todo);
+    req.session.thingsToDo.push(req.body.todo);
+    res.render('todo.ejs', { thingsToDo: req.session.thingsToDo });
+})
+.get('/todo/delete/:index', (req, res, next) => {
+    console.log(req.params.index);
+    req.session.thingsToDo.splice(req.params.index, 1);
     res.render('todo.ejs', { thingsToDo: req.session.thingsToDo });
 })
 
